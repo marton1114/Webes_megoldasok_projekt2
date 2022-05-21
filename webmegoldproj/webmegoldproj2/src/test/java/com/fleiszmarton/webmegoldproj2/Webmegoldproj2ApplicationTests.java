0f5b600e4bdba82a191d139031dc52aa300bd249
marton1114 <fleiszmarci@gmail.com>
2022-05-21 16:00:38 +0300

    v1 commit

---
+++ webmegoldproj/webmegoldproj2/src/test/java/com/fleiszmarton/webmegoldproj2/Webmegoldproj2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleiszmarton.webmegoldproj2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webmegoldproj2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
