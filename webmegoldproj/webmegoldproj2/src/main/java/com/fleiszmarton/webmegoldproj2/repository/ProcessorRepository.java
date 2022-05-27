49a5747ff8a0ebd6f170c126613d3b442593bfe0
marton1114 <fleiszmarci@gmail.com>
2022-05-27 10:12:54 +0300

    elolrol

--- webmegoldproj/webmegoldproj2/src/main/java/com/fleiszmarton/webmegoldproj2/repository/ProcessorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fleiszmarton.webmegoldproj2.repository;
-
-import com.fleiszmarton.webmegoldproj2.model.components.Processor;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProcessorRepository extends CrudRepository<Processor, Integer> {
-}
